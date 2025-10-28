f674b77769c4510ec52dd8199359073317ed189d
Amaurys De Los Santos Mendez <amaurysdlsm@gmail.com>
2025-10-27 23:43:03 -0400

    feat: include uploadedBy and uploadedById in file metadata during upload

--- src/app/api/trainings/[id]/files/route.ts
+++ src/app/api/trainings/[id]/files/route.ts
@@ -63,6 +63,8 @@ export async function POST(
       fileName: file.name,
       fileType: file.type,
       url,
+      uploadedBy,
+      uploadedById,
     });
 
     return NextResponse.json(newFile);
