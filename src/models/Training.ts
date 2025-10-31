ef49a0f2463b9fad139e037000c5f41c9a631b59
Amaurys De Los Santos Mendez <amaurysdlsm@gmail.com>
2025-10-30 22:50:46 -0400

    refactor: Comment out Training model import and related schema in index file

--- src/models/Training.ts
+++ src/models/Training.ts
@@ -1,24 +1,24 @@
-// src/models/Training.ts
-import mongoose, { Schema, models } from "mongoose";
-import { ITraining } from "./mongoTypes";
+// // src/models/Training.ts
+// import mongoose, { Schema, models } from "mongoose";
+// import { ITraining } from "./mongoTypes";
 
-const TrainingSchema = new Schema<ITraining>(
-  {
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    module: {
-      type: Schema.Types.ObjectId,
-      ref: "TrainingModule",
-      required: true,
-    },
-    // ojt: { type: Boolean, default: false },
-    // practical: { type: Boolean, default: false },
-    // signedOff: { type: Boolean, default: false },
-    // signatures: [{ type: Schema.Types.ObjectId, ref: "Signature" }], // Array of IDs
-    notes: { type: String, default: "" },
-  },
-  { timestamps: true }
-);
+// const TrainingSchema = new Schema<ITraining>(
+//   {
+//     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+//     module: {
+//       type: Schema.Types.ObjectId,
+//       ref: "TrainingModule",
+//       required: true,
+//     },
+//     // ojt: { type: Boolean, default: false },
+//     // practical: { type: Boolean, default: false },
+//     // signedOff: { type: Boolean, default: false },
+//     // signatures: [{ type: Schema.Types.ObjectId, ref: "Signature" }], // Array of IDs
+//     notes: { type: String, default: "" },
+//   },
+//   { timestamps: true }
+// );
 
-export default models.Training ||
-  mongoose.model<ITraining>("Training", TrainingSchema);
-export type { ITraining };
+// export default models.Training ||
+//   mongoose.model<ITraining>("Training", TrainingSchema);
+// export type { ITraining };
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,9 @@ import UserModule from './UserModule';
 import UserSubmodule from './UserSubmodule';
 // Import in order to ensure proper registration
 import TrainingModule from "./TrainingModule";
-import Training from "./Training";
 import User from "./User";
 import Signature from "./Signature";
 import TrainingSubModule from "./TrainingSubModule";
 // Re-export everything
-export { TrainingModule, Training, User, Signature, TrainingSubModule, UserSubmodule, UserModule };
+export { TrainingModule, User, Signature, TrainingSubModule, UserSubmodule, UserModule };
 export * from "./mongoTypes";
